b31e4d426a4ad0a08fa69a9c95b9dcaad05f3eea
Shantaram K <shantaramk@mipladmin-67.local>
2019-10-25 11:33:35 +0530

    Initial Commit

---
+++ XMLParsingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XMLParsingDemo
+//
+//  Created by Shantaram K on 25/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
